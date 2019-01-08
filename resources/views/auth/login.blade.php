7bf08054dc4790911cb905a846e9c72423e2cccf
Fathur Rahman <fathur@nazrol.tech>
2019-01-08 16:36:28 +0800

    chore: Update login blade to follow Laravel auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,58 +25,59 @@
             <!-- END Header -->
 
             <!-- Sign In Form -->
-            <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.min.js which was auto compiled from _es6/pages/op_auth_signin.js) -->
-            <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
-            <form class="js-validation-signin" action="be_pages_auth_all.html" method="POST">
+
+            <form class="js-validation-signin" action="{{ route('login') }}" method="POST">
+                @csrf
                 <div class="form-group">
                     <div class="input-group">
-                        <input type="text" class="form-control" id="login-username" name="login-username" placeholder="Username">
+                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="{{ __('E-Mail Address') }}" required autofocus>
                         <div class="input-group-append">
-                                                    <span class="input-group-text">
-                                                        <i class="fa fa-user-circle"></i>
-                                                    </span>
+                            <span class="input-group-text">
+                                <i class="fa fa-user-circle"></i>
+                            </span>
                         </div>
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                        @endif
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="input-group">
-                        <input type="password" class="form-control" id="login-password" name="login-password" placeholder="Password">
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" required>
                         <div class="input-group-append">
-                                                    <span class="input-group-text">
-                                                        <i class="fa fa-asterisk"></i>
-                                                    </span>
+                            <span class="input-group-text">
+                                <i class="fa fa-asterisk"></i>
+                            </span>
                         </div>
+
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                        @endif
                     </div>
                 </div>
                 <div class="form-group d-sm-flex justify-content-sm-between align-items-sm-center text-center text-sm-left">
                     <div class="custom-control custom-checkbox custom-control-primary">
-                        <input type="checkbox" class="custom-control-input" id="login-remember-me" name="login-remember-me" checked>
-                        <label class="custom-control-label" for="login-remember-me">Remember Me</label>
+                        <input class="custom-control-input" type="checkbox" name="remember" id="login-remember-me" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="custom-control-label" for="login-remember-me">{{ __('Remember Me') }}</label>
                     </div>
+
                     <div class="font-w600 font-size-sm py-1">
-                        <a href="javascript:void(0)">Forgot Password?</a>
+                        @if (Route::has('password.request'))
+                            <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                        @endif
                     </div>
                 </div>
                 <div class="form-group text-center">
                     <button type="submit" class="btn btn-hero-primary">
-                        <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Sign In
+                        <i class="fa fa-fw fa-sign-in-alt mr-1"></i> {{ __('Login') }}
                     </button>
                 </div>
             </form>
             <!-- END Sign In Form -->
-        </div>
-        <div class="block-content bg-body">
-            <div class="d-flex justify-content-center text-center push">
-                <a class="item item-circle item-tiny mr-1 bg-default" data-toggle="theme" data-theme="default" href="#"></a>
-                <a class="item item-circle item-tiny mr-1 bg-xwork" data-toggle="theme" data-theme="assets/css/themes/xwork.min.css" href="#"></a>
-                <a class="item item-circle item-tiny mr-1 bg-xmodern" data-toggle="theme" data-theme="assets/css/themes/xmodern.min.css" href="#"></a>
-                <a class="item item-circle item-tiny mr-1 bg-xeco" data-toggle="theme" data-theme="assets/css/themes/xeco.min.css" href="#"></a>
-                <a class="item item-circle item-tiny mr-1 bg-xsmooth" data-toggle="theme" data-theme="assets/css/themes/xsmooth.min.css" href="#"></a>
-                <a class="item item-circle item-tiny mr-1 bg-xinspire" data-toggle="theme" data-theme="assets/css/themes/xinspire.min.css" href="#"></a>
-                <a class="item item-circle item-tiny mr-1 bg-xdream" data-toggle="theme" data-theme="assets/css/themes/xdream.min.css" href="#"></a>
-                <a class="item item-circle item-tiny mr-1 bg-xpro" data-toggle="theme" data-theme="assets/css/themes/xpro.min.css" href="#"></a>
-                <a class="item item-circle item-tiny bg-xplay" data-toggle="theme" data-theme="assets/css/themes/xplay.min.css" href="#"></a>
-            </div>
         </div>
     </div>
     <!-- END Sign In Block -->
