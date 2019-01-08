826079648f28468100969002cb111bcb5d0acd81
Fathur Rahman <fathur@nazrol.tech>
2019-01-08 15:25:30 +0800

    chore: Auth page blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,84 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('js_after')
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+
+    <!-- Page JS Code -->
+
+    <script src="{{ asset('js/pages/op_auth_signin.min.js') }}"></script>
+
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Sign In Block -->
+    <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
+        <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-white">
+            <!-- Header -->
+            <div class="mb-2 text-center">
+                <a class="link-fx font-w700 font-size-h1" href="index.html">
+                    <span class="text-dark">Dash</span><span class="text-primary">mix</span>
+                </a>
+                <p class="text-uppercase font-w700 font-size-sm text-muted">Sign In</p>
+            </div>
+            <!-- END Header -->
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+            <!-- Sign In Form -->
+            <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.min.js which was auto compiled from _es6/pages/op_auth_signin.js) -->
+            <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
+            <form class="js-validation-signin" action="be_pages_auth_all.html" method="POST">
+                <div class="form-group">
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="login-username" name="login-username" placeholder="Username">
+                        <div class="input-group-append">
+                                                    <span class="input-group-text">
+                                                        <i class="fa fa-user-circle"></i>
+                                                    </span>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <input type="password" class="form-control" id="login-password" name="login-password" placeholder="Password">
+                        <div class="input-group-append">
+                                                    <span class="input-group-text">
+                                                        <i class="fa fa-asterisk"></i>
+                                                    </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group d-sm-flex justify-content-sm-between align-items-sm-center text-center text-sm-left">
+                    <div class="custom-control custom-checkbox custom-control-primary">
+                        <input type="checkbox" class="custom-control-input" id="login-remember-me" name="login-remember-me" checked>
+                        <label class="custom-control-label" for="login-remember-me">Remember Me</label>
+                    </div>
+                    <div class="font-w600 font-size-sm py-1">
+                        <a href="javascript:void(0)">Forgot Password?</a>
+                    </div>
+                </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-hero-primary">
+                        <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Sign In
+                    </button>
+                </div>
+            </form>
+            <!-- END Sign In Form -->
+        </div>
+        <div class="block-content bg-body">
+            <div class="d-flex justify-content-center text-center push">
+                <a class="item item-circle item-tiny mr-1 bg-default" data-toggle="theme" data-theme="default" href="#"></a>
+                <a class="item item-circle item-tiny mr-1 bg-xwork" data-toggle="theme" data-theme="assets/css/themes/xwork.min.css" href="#"></a>
+                <a class="item item-circle item-tiny mr-1 bg-xmodern" data-toggle="theme" data-theme="assets/css/themes/xmodern.min.css" href="#"></a>
+                <a class="item item-circle item-tiny mr-1 bg-xeco" data-toggle="theme" data-theme="assets/css/themes/xeco.min.css" href="#"></a>
+                <a class="item item-circle item-tiny mr-1 bg-xsmooth" data-toggle="theme" data-theme="assets/css/themes/xsmooth.min.css" href="#"></a>
+                <a class="item item-circle item-tiny mr-1 bg-xinspire" data-toggle="theme" data-theme="assets/css/themes/xinspire.min.css" href="#"></a>
+                <a class="item item-circle item-tiny mr-1 bg-xdream" data-toggle="theme" data-theme="assets/css/themes/xdream.min.css" href="#"></a>
+                <a class="item item-circle item-tiny mr-1 bg-xpro" data-toggle="theme" data-theme="assets/css/themes/xpro.min.css" href="#"></a>
+                <a class="item item-circle item-tiny bg-xplay" data-toggle="theme" data-theme="assets/css/themes/xplay.min.css" href="#"></a>
             </div>
         </div>
     </div>
-</div>
+    <!-- END Sign In Block -->
+
 @endsection
