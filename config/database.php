93e67d9d4145bbd926e1a0c7451c0d2419c1ff02
Fathur Rahman <fathur@nazrol.tech>
2019-01-18 11:06:26 +0800

    chore: setup Laravel Dusk to use sqlite on local test

--- config/database.php
+++ config/database.php
@@ -33,6 +33,12 @@ return [
 
     'connections' => [
 
+        'dusk_sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => database_path('database.sqlite'),
+            'prefix'   => '',
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
