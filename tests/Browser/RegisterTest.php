b5c8f289a79d650873e05bd36de2ad4945896155
Fathur Rahman <fathur@nazrol.tech>
2019-01-18 12:57:50 +0800

    fix: improve Dusk register test

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -2,43 +2,37 @@
 
 namespace Tests\Browser;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\User;
 
 class RegisterTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    
-    public function testRegister()
+    use DatabaseMigrations;
+
+    public function testViewRegister()
     {
-        $user = factory(User::class)->create();
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                ->assertSee('SIGN UP');
+        });
+    }
+    
+    public function testUserCanRegister()
+    {
+        $user = factory(User::class)->make();
+
         $this->browse(function (Browser $browser) use ($user) {
             $browser
                 ->visit('/register')
                 ->assertSee('SIGN UP')
                 ->type('name', $user->name)
                 ->type('email', $user->email)
-                ->type('password', 'password')
-                ->type('password_confirmation', 'password')
+                ->type('password', 'secret')
+                ->type('password_confirmation', 'secret')
                 ->press('SIGN UP')
-                ->visit('/logout')
-                ->logout()
-                ->visit('/login')
-                ->assertSee('SIGN IN');    
-        });
-    }
-
-    public function testViewRegister()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/register')
-                    ->assertSee('SIGN UP');
+                ->logout();
         });
     }
 
