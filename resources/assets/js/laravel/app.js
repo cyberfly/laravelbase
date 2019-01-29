00ceee455571f7ba781b8882dab3ad51aac22b3c
Fathur Rahman <fathur@nazrol.tech>
2019-01-29 15:01:44 +0800

    feat: Parent Validation Alert component, to show validation errors on multi form component

--- resources/assets/js/laravel/app.js
+++ resources/assets/js/laravel/app.js
@@ -58,6 +58,7 @@ Vue.component('show-user-component', require('../../../components/admin/users/Sh
 Vue.component('v-text', require('../../../components/common/forms/InputTextComponent.vue'));
 Vue.component('v-select', require('../../../components/common/forms/SelectInputComponent.vue'));
 Vue.component('v-validation-alert', require('../../../components/common/forms/ValidationAlertComponent.vue'));
+Vue.component('v-parent-validation-alert', require('../../../components/common/forms/ParentValidationAlertComponent.vue'));
 
 /*
  End of Common Components
---
+++ resources/components/common/forms/ParentValidationAlertComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-if="submitted && errors.items.length" class="alert alert-danger alert-dismissable" role="alert" >
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <h3 class="alert-heading font-size-h4 my-2">Submission Error</h3>
+        <p class="mb-0">There is an error with your submission. Please fix all the error below to continue.</p>
+        <ul v-if="show_error_details">
+            <li v-for="(el, key) in errors.items" >{{ el.msg }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            submitted: {
+                default: false,
+                required: true
+            },
+            show_error_details: {
+                default: true
+            }
+        },
+        mounted() {
+
+        },
+        computed: {
+
+        },
+        methods: {
+
+        }
+    }
+
+</script>
