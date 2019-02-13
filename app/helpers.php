aef33aa0626c61e561c15e455f6e0033aabf8c7c
Fathur Rahman <fathur@nazrol.tech>
2019-02-13 17:23:30 +0800

    feat: custom date and money helper

--- app/helpers.php
+++ app/helpers.php
@@ -4,6 +4,8 @@
  *  Custom Laravel Helpers
  */
 
+use Akaunting\Money\Currency;
+use Akaunting\Money\Money;
 use Illuminate\Support\Carbon;
 
 if (! function_exists('carbon')) {
@@ -11,4 +13,33 @@ if (! function_exists('carbon')) {
     {
         return new Carbon($parseString, $tz);
     }
-}   
+}
+
+if (! function_exists('my_date')) {
+    function my_date($date)
+    {
+        if (empty($date)) {
+            return '-';
+        }
+
+        return Carbon::parse($date)->format('d/m/Y');
+    }
+}
+
+if (! function_exists('my_datetime')) {
+    function my_datetime($date)
+    {
+        if (empty($date)) {
+            return '-';
+        }
+
+        return Carbon::parse($date)->format('d/m/Y H:i:s');
+    }
+}
+
+if (! function_exists('my_money')) {
+    function my_money($amount)
+    {
+        return new Money($amount, new Currency('MYR'));
+    }
+}
