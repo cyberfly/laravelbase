a4b48614eeaedacefb9d7e00fe106b71b7c158ea
Fathur Rahman <fathur@nazrol.tech>
2019-03-06 17:09:22 +0800

    feat: integrate Users breadcrumb into blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,7 @@
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Dashboard</h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item">App</li>
-                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-                    </ol>
+                    {{ Breadcrumbs::render('dashboard') }}
                 </nav>
             </div>
        </div>
