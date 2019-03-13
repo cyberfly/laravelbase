4986e3e1858c63f2f2806be622594defb27cbb27
Fathur Rahman <fathur@nazrol.tech>
2019-03-13 10:28:34 +0800

    chore: custom messages language file

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'validation_error_header' => 'Validation Error',
+    'validation_error_message' => 'There is an error with your submission. Please fix the errors below to continue.',
+];
---
+++ resources/lang/ms/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'validation_error_header' => 'Gagal Validasi',
+    'validation_error_message' => 'Terdapat masalah validasi pada borang ini. Sila betulkan untuk maju ke hadapan.',
+];
