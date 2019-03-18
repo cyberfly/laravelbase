f710989c7c86c3651a9d65d871f6249806dc5698
Fathur Rahman <fathur@nazrol.tech>
2019-03-18 10:32:56 +0800

    chore: update labels lang

--- resources/lang/en/labels.php
+++ resources/lang/en/labels.php
@@ -1,11 +1,28 @@
 <?php
 
 return [
-    'submit' => 'Submit',
-    'save' => 'Save',
-    'cancel' => 'Cancel',
-    'reset' => 'Reset',
-    'next' => 'Next',
+    'submit'   => 'Submit',
+    'save'     => 'Save',
+    'cancel'   => 'Cancel',
+    'reset'    => 'Reset',
+    'next'     => 'Next',
     'previous' => 'Previous',
-    'select' => 'Select',
+    'select'   => 'Select',
+
+    // auth label
+
+    'sign_in'              => 'Sign In',
+    'login'                => 'Login',
+    'logout'               => 'Logout',
+    'remember_me'          => 'Remember Me',
+    'forgot_your_password' => 'Forgot Your Password?',
+    'email'                => 'Email',
+    'password'             => 'Password',
+
+    // control panel label
+
+    'user_options'  => 'User Options',
+    'notifications' => 'Notifications',
+    'profile'       => 'Profile',
+    'inbox'         => 'Inbox',
 ];
--- resources/lang/ms/labels.php
+++ resources/lang/ms/labels.php
@@ -1,11 +1,28 @@
 <?php
 
 return [
-    'submit' => 'Hantar',
-    'save' => 'Simpan',
-    'cancel' => 'Batal',
-    'reset' => 'Padam',
-    'next' => 'Seterusnya',
+    'submit'   => 'Hantar',
+    'save'     => 'Simpan',
+    'cancel'   => 'Batal',
+    'reset'    => 'Padam',
+    'next'     => 'Seterusnya',
     'previous' => 'Kembali',
-    'select' => 'Pilih',
+    'select'   => 'Pilih',
+
+    // auth label
+
+    'sign_in'              => 'Log Masuk',
+    'login'                => 'Log Masuk',
+    'logout'               => 'Log Keluar',
+    'remember_me'          => 'Ingat Saya',
+    'forgot_your_password' => 'Lupa Kata Laluan?',
+    'email'                => 'Emel',
+    'password'             => 'Kata Laluan',
+
+    // control panel label
+
+    'user_options'  => 'Pilihan Pengguna',
+    'notifications' => 'Pemberitahuan',
+    'profile'       => 'Profile',
+    'inbox'         => 'Inbox',
 ];
