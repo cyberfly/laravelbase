3ba958b0c68c4dac3cd7fbf90b47603a3ab741ef
Fathur Rahman <fathur@nazrol.tech>
2019-03-26 16:10:14 +0800

    feat: hasChildRelations() helpers

--- app/helpers.php
+++ app/helpers.php
@@ -41,3 +41,27 @@ if (! function_exists('my_money')) {
         return money($amount, 'MYR', true);
     }
 }
+
+if (! function_exists('hasChildRelations')) {
+    /**
+     * Check if object has child relations count.
+     * To load the relationship count into the object, use withCount()
+     * @param $object
+     * @return bool
+     */
+    function hasChildRelations($object)
+    {
+        $object = $object->toArray();
+
+        foreach ($object as $key => $value) {
+
+            if (preg_match("/_count/i", $key)) {
+                if ($value > 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
