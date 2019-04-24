84af437e7985806f23bd976c87a426cebf4ac6b0
Fathur Rahman <fathur@nazrol.tech>
2019-04-24 15:18:16 +0800

    chore: new global method delete form and delete success notification

--- resources/assets/js/mixins/Commons.vue
+++ resources/assets/js/mixins/Commons.vue
@@ -7,6 +7,45 @@
                 return route(route_name, params);
             },
 
+            // show confirmation to delete Vue generated row
+            $showDeleteForm(id, index) {
+
+                this.$swal({
+                    title: 'Are you sure to delete this record?',
+                    text: 'This action cannot be undo.',
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Yes',
+                    cancelButtonText: 'Cancel',
+                    showLoaderOnConfirm: true,
+                }).then((result) => {
+
+                    if(result.value) {
+
+                        this.destroy(id, index);
+                    } else {
+
+                    }
+                });
+
+            },
+
+            // show success deleted alert
+
+            $showSuccessDeleted(label) {
+
+                this.$swal({
+                    title: 'Deleted',
+                    text: label + ' was successfully deleted.',
+                    type: 'success',
+                    confirmButtonText: 'Ok',
+                    timer: 2000,
+                }).then(function() {
+
+
+                });
+            },
+
         },
     };
 </script>
