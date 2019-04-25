6f74a2d66e4485561b4722a79fefd25cd5656dd8
Fathur Rahman <fathur@nazrol.tech>
2019-04-25 17:02:01 +0800

    feat: add fade in effect to validation error component

--- resources/components/common/forms/ValidationAlertComponent.vue
+++ resources/components/common/forms/ValidationAlertComponent.vue
@@ -1,14 +1,16 @@
 <template>
-    <div v-if="submitted && errors.any()" class="alert alert-danger alert-dismissable" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        <h3 class="alert-heading font-size-h4 my-2">{{ trans.get('messages.validation_error_header') }}</h3>
-        <p class="mb-0">{{ trans.get('messages.validation_error_message') }}</p>
-        <ul>
-            <li v-for="error in errors.all()">{{ error }}</li>
-        </ul>
-    </div>
+    <transition name="fade">
+        <div v-if="submitted && errors.any()" class="alert alert-danger alert-dismissable" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <h3 class="alert-heading font-size-h4 my-2">{{ trans.get('messages.validation_error_header') }}</h3>
+            <p class="mb-0">{{ trans.get('messages.validation_error_message') }}</p>
+            <ul>
+                <li v-for="error in errors.all()">{{ error }}</li>
+            </ul>
+        </div>
+    </transition>
 </template>
 
 <script>
@@ -34,3 +36,12 @@
     }
 
 </script>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
+</style>
