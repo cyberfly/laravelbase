9f6dc94602d84fea5e77f15440362a8d216cab7f
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2019-05-14 11:28:40 +0800

    feat: update Html List Component to support excerpt

--- resources/components/common/lists/HtmlListComponent.vue
+++ resources/components/common/lists/HtmlListComponent.vue
@@ -1,9 +1,45 @@
 <template>
 
     <div>
+
         <ul class="v-html-list">
-            <li class="" v-for="row in list_data">{{ row[label_key] }}</li>
+            <li class="" v-for="(row, index) in list_data_excerpt" :key="index" >{{ row[label_key] }}</li>
         </ul>
+
+        <template v-if="list_data_balance > 0">
+            <button @click="showModal()" class="btn btn-sm btn-info">+ {{ list_data_balance }}</button>
+
+            <!--modal-->
+
+            <b-modal ref="actionModalRef"
+                     size="lg"
+                     :title="list_title"
+                     :header-class="'bg-primary-dark'"
+                     :header-text-variant="'light'"
+            >
+                <div class="d-block">
+
+                    <div class="form-row">
+
+                        <div class="form-group col-md-12">
+
+                            <ul class="v-html-list">
+                                <li class="" v-for="(row, index) in list_data" :key="index" >{{ row[label_key] }}</li>
+                            </ul>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div slot="modal-footer">
+
+                </div>
+            </b-modal>
+
+            <!--end modal-->
+        </template>
     </div>
 
 </template>
@@ -16,12 +52,29 @@
                 type: Array,
                 required: true,
             },
+            list_title: {
+                required: true,
+            },
             label_key: {
                 type: String,
                 required: true,
+            },
+            limit: {
+                default: 5,
             }
         },
         mounted() {
+
+        },
+        computed:{
+
+            list_data_excerpt() {
+                return this.limit ? this.list_data.slice(0, this.limit) : this.list_data;
+            },
+
+            list_data_balance() {
+                return this.list_data.length - this.limit;
+            },
 
         },
         data () {
@@ -35,6 +88,9 @@
         },
         methods: {
 
+            showModal() {
+                this.$refs.actionModalRef.show();
+            },
         }
     }
 </script>
